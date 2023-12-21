8e0c767dc8a7e9fed7dc3e09def45f0ff71d4325
Bobby Mannino <manninobobby@icloud.com>
2023-12-21 17:32:56 +0000

    changes

--- 04-srgui/mainwindow.cpp
+++ 04-srgui/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +15,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_action_Open_Database_triggered()
+{
+    QString selectedFilePath = QFileDialog::getOpenFileName(this, tr("Open Database"), QDir::homePath(), tr("Database Files (*.txt)"));
+
+    QFile file(selectedFilePath);
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream in(&file);
+        QString fileContents = in.readAll();
+        file.close();
+
+        ui->nameLabel->setText(fileContents);
+    } else {
+        QMessageBox::information(nullptr, "Alert", "Failed to open file");
+    }
+}
